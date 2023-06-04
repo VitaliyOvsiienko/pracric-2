c047d24c304b9a333c743b01bcfa410bec69d297
Nadiia Tsytsylina <miniova95@gmail.com>
2023-06-04 12:18:12 +0300

    Update Gallery.jsx

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -7,35 +7,82 @@ export class Gallery extends Component {
   state = {
     query: '',
     images: [],
-    page:1
-  }
+    page: 1,
+    isEmpty: false,
+    isLoading: false,
+    isShowButton: false,
+    error: null,
+  };
 
   componentDidUpdate(prevProps, prevState) {
-    const {query, page}=this.state
-    if (prevState.query !== query) {
-      this.getPhotos(query, page)
-   }
-   console.log(this.state)
- }
-
-
-
-  handleSubmitQuery = (name) => {
-    this.setState({ query: name })
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getPhotos(query, page);
+    }
   }
 
-  getPhotos = async(query, page) => {
-    const {photos} = await ImageService.getImages(query, page);
-    
-    this.setState(({ images }) => ({ images: [...images, ...photos] }))
-    
-  }
+  handleSubmitQuery = name => {
+    this.setState({ query: name, images: [], isEmpty: false, page: 1 });
+  };
+
+  handleClickButton = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  getPhotos = async (query, page) => {
+    this.setState({ isLoading: true });
+    try {
+      const {
+        photos,
+        total_results,
+        per_page,
+        page: currentPage,
+      } = await ImageService.getImages(query, page);
+      if (photos.length === 0) {
+        this.setState({ isEmpty: true });
+        return;
+      }
+      this.setState(({ images }) => ({
+        images: [...images, ...photos],
+        isShowButton: currentPage < Math.ceil(total_results / per_page),
+      }));
+    } catch (error) {
+      console.error(error);
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
 
   render() {
+    const { images, isEmpty, isLoading, isShowButton, error } = this.state;
+    const isShowImages = images.length > 0;
     return (
       <>
-        <SearchForm onSubmit={ this.handleSubmitQuery} />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm onSubmit={this.handleSubmitQuery} />
+        {isShowImages && (
+          <Grid>
+            {images.map(({ src: { medium }, alt, id }) => {
+              return (
+                <GridItem key={id}>
+                  <CardItem>
+                    <img src={medium} alt={alt} />
+                  </CardItem>
+                </GridItem>
+              );
+            })}
+          </Grid>
+        )}
+        {isEmpty && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
+        {isLoading && <Text textAlign="center">Loading ...</Text>}
+        {isShowButton && (
+          <Button type="button" onClick={this.handleClickButton}>
+            Load more
+          </Button>
+        )}
+        {error && <Text textAlign="center">{error}</Text>}
       </>
     );
   }
